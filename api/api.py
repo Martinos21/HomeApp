bda6a950f8e4a116a1682bdbc9e95fb2a722418e
Martehn03 <encantr@seznam.cz>
2025-10-14 22:36:15 +0200

    add database support

--- api/api.py
+++ api/api.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 @app.route('/data', methods=['POST'])
 def data():
     d = request.json
+    for key, value in d.items():
+        cur = con.cursor()
+        cur.execute(f"CREATE TABLE IF NOT EXISTS {key} (value)")
+        con.commit()
+        cur.execute(f"INSERT INTO {key} VALUES ({value})")
+        con.commit()
+        cur.close()
     print(d)
     return "OK"
 
 if __name__ == "__main__":
     # Run Flask server
+    con = sqlite3.connect('home.db')
     app.run(host="0.0.0.0", port=8070)
